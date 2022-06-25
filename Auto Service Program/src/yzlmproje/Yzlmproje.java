c193366872f89b181c294dcd5be5ba6f2e310e46
Yigitcan Keskin <yc.keskin.1903@gmail.com>
2022-06-25 16:57:19 +0300

    Service Added

---
+++ Auto Service Program/src/yzlmproje/Yzlmproje.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yzlmproje;
+
+/**
+ *
+ * @author yc_ke
+ */
+public class Yzlmproje {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        loginPage lp = new loginPage();
+        lp.show();
+    }
+    
+}
